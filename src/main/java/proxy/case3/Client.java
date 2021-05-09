51ab120b04366557278ee9093b96b34f14f7bd4c
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2021-05-10 00:02:14 +0900

    Proxy case 추가

---
+++ src/main/java/proxy/case3/Client.java
@@ -0,0 +1,34 @@
+package proxy.case3;
+
+public class Client {
+    // https://refactoring.guru/design-patterns/proxy/java/example
+    public static void main(String[] args) {
+        YouTubeDownloader naiveDownloader = new YouTubeDownloader(new ThirdPartyYouTubeImp());
+        YouTubeDownloader smartDownloader = new YouTubeDownloader(new YouTubeCacheProxy());
+
+        long naive = test(naiveDownloader);
+        long smart = test(smartDownloader);
+
+        System.out.print("Time saved by caching proxy: " + (naive - smart) + "ms");
+    }
+
+    private static long test(YouTubeDownloader downloader) {
+        final long startTime = System.currentTimeMillis();
+
+        // User behavior in our app:
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("catzzzzzzzzz");
+
+        downloader.renderPopularVideos();
+        downloader.renderVideoPage("dancesvideoo");
+
+        // Users might visit the same page quite often.
+        downloader.renderVideoPage("catzzzzzzzzz");
+        downloader.renderVideoPage("someothervid");
+
+        final long estimatedTime = System.currentTimeMillis() - startTime;
+        System.out.print("Time elapsed: " + estimatedTime + "ms\n");
+
+        return estimatedTime;
+    }
+}
---
+++ src/main/java/proxy/case3/Video.java
@@ -0,0 +1,26 @@
+package proxy.case3;
+
+public class Video {
+
+    private String id;
+    private String title;
+    private String data;
+
+    public Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random Video.";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
