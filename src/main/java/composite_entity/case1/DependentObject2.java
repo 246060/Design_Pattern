5184a9a5e296569fcbd70b1f859951e7faa7d77b
blackcocoon <42939960+blackcocoon@users.noreply.github.com>
2022-09-26 03:54:52 +0900

    composite entity pattern 예제 추가

---
+++ src/main/java/composite_entity/case1/DependentObject2.java
@@ -0,0 +1,14 @@
+package composite_entity.case1;
+
+public class DependentObject2 {
+
+	private String data;
+
+	public void setData(String data){
+		this.data = data;
+	}
+
+	public String getData(){
+		return data;
+	}
+}
